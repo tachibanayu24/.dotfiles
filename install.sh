aff3371897676aeac385b160092eac5b64beb4e3
Yuto Tachibana <tachibanayu24@gmail.com>
2019-08-12 04:10:58 +0900

    .item2

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ DOTPATH=~/.dotfiles
 for f in .??*
 do
     [[ "$f" = ".git" ]] && continue
+    [[ "$f" = ".gitmodules" ]] && continue
     [[ "$f" == ".DS_Store" ]] && continue
 
     ln -snfv "$DOTPATH/$f" "$HOME"/"$f"
